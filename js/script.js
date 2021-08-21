085fbf139041e2b677dc1db32307c316e942ae51
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-08-21 16:25:29 +0700

    Revision

--- js/script.js
+++ js/script.js
@@ -1,29 +1,62 @@
 //Get the button
 let mybutton = document.getElementById("btn-back-to-top");
+let myModal = new bootstrap.Modal(document.getElementById("modalKu"));
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-  scrollFunction();
+	scrollFunction();
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+	if (
+		document.body.scrollTop > 20 ||
+		document.documentElement.scrollTop > 20
+	) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
 }
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
 
+function showModal(jumlah, typeBtn = "", wemail = "", goods = "") {
+	let h = document.querySelector("#jenis");
+	let we = document.querySelector("#wemail");
+	let g = document.querySelector("#goods");
+	h.value = typeBtn;
+	we.value = wemail;
+	g.value = goods;
+
+	let j = document.querySelector(".activeJumlah");
+	if (!jumlah) {
+		j.setAttribute("hidden", "true");
+		return myModal.show();
+	}
+
+	j.removeAttribute("hidden");
+	myModal.show();
+}
+
+function evalForm(f) {
+	let nama = f.pengirim.value ? f.pengirim.value : "Anonim";
+	let alamat = f.alamat.value ? f.alamat.value : "Indonesia";
+	let jenis = f.jenis.value;
+	let we = f.wemail.value;
+	let g = f.goods.value ? f.goods.value : "barang elektronik";
+	let n = f.jumlah.value ? f.jumlah.value : false;
+
+	return hubungi(nama, alamat, jenis, g, n, we);
+}
+
+function s(url) {
+	return console.log(url);
+}
 
 // Modal
 
@@ -33,45 +66,25 @@ function backToTop() {
 
 // Service Page
 // Navbar btn WA, Maps Section WA btn, Menu Bottom (Hubungi Kami)
-function hubungiWa() {
-  // Halo, saya <nama> beralamat di <alamat>.
-  // Saya ingin <layanan> <nama barang>.
-  // Layanan:
-  // - Service atau Perbaikan = melakukan service atau perbaikan <nama barang>.
-  // - Membeli Barang = membeli <nama barang>. Apakah tersedia? Dan berapa harganya?
-  // - Menjual Barang = menjual <nama barang> ke Benadem Service.
-  alert('WA')
-}
+function hubungi(na, a, j, g, n, we) {
+	let def;
+	if (we == "w") {
+		def = "https://api.whatsapp.com/send?phone=6281283051589&text=";
+	} else {
+		def = "mailto:agungdp2799@gmail.com?subject=";
+	}
 
-// Halaman Utama
-// Maps Section Email btn
-function hubungiEmail() { 
-  // Halo, saya <nama> beralamat di <alamat>.
-  // Saya ingin <layanan> <nama barang>.
-  // Layanan:
-  // - Service atau Perbaikan = melakukan service atau perbaikan <nama barang>.
-  // - Membeli Barang = membeli <nama barang>. Apakah tersedia? Dan berapa harganya?
-  // - Menjual Barang = menjual <nama barang> ke Benadem Service.
-  alert('EMAIL')
-}
+	let ine = `Halo, Benadem Service! Saya ${na} beralamat di ${a}. Saya ingin `;
+	let sell = `menjual ${n ? n + " " : ""}${g} ke Benadem Service.`;
+	let buy = `membeli ${
+		n ? n + " " : ""
+	}${g}. Apakah tersedia? Dan berapa harganya?`;
+	let serv = `melakukan service atau perbaikan ${g}`;
 
-// Halaman Utama
-// Section Produk
-function beliProduk(){
-  // Pesan:
-  // Halo, saya <nama> beralamat di <alamat>.
-  // Saya ingin membeli <nama barang - get name>. Apakah tersedia?  // Produk terakhir = lainnya. Tanpa nama barang.
-  // Saya ingin tahu lebih detail barang yang dijual.
-  alert('BELI BARANG')
-}
-
-// Halaman Utama
-// Carousel slide 2, Menu Bottom (Jual Barang)
-function jualBarang(){
-  // Pesan:
-  // Halo, saya <nama> beralamat di <alamat>.
-  // Saya ingin menjual <nama barang> ke Benadem Service.
-  alert('JUAL BARANG')
+	if (j === "service") return s(def + ine + serv);
+	if (j === "sell") return s(def + ine + sell);
+	if (j === "buy") return s(def + ine + buy);
+	return s(def + ine + "bertanya-tanya mengenai Benadem Service.");
 }
 
 // Halaman Utama
@@ -79,23 +92,21 @@ function jualBarang(){
 
 // Service Page
 // Hero
-function gunakanJasa(){
-  // Pesan:
-  // Halo, saya <nama> beralamat di <alamat>. 
-  // Saya ingin menggunakan jasa service Benadem Service.
-  alert('GUNAKAN JASA')
+function gunakanJasa() {
+	// Pesan:
+	// Halo, saya <nama> beralamat di <alamat>.
+	// Saya ingin menggunakan jasa service Benadem Service.
+	alert("GUNAKAN JASA");
 }
 
 // Section Page
 // Card Service
-function jasaService(){
-  // Pesan:
-  // Halo, saya <nama> beralamat di <alamat>. 
-  // Saya ingin menggunakan jasa service Benadem Service untuk <nama service - get name>
-  alert('SERVICE')
+function jasaService() {
+	// Pesan:
+	// Halo, saya <nama> beralamat di <alamat>.
+	// Saya ingin menggunakan jasa service Benadem Service untuk <nama service - get name>
+	alert("SERVICE");
 }
-
 
 // FILTER KATEGORI SERVICE
 // PENCARIAN SERVICE
-
